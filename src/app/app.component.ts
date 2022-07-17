3c68706102e9fdb4b1048a21e49a8374df032992
Nimeshkumar Italiya <=>
2022-07-17 21:32:39 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { catchError, map, Observable, throwError } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+
+export class AppComponent implements OnInit {
+  title = 'poc-app';
+  public url: any;
+  public isError: Boolean = false;
+  public response: any;
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+  }
+
+  async api_call() {
+    this.isError = false;
+    await (await this.make_call("http://localhost:5000" + this.url)).subscribe((data) => {
+      this.response = data;
+    }, (err) => {
+      this.isError = true;
+      this.response = err;
+    }
+    );
+  }
+
+  make_call = async (url: any) => {
+    return this.http.get(url);
+  }
+}
